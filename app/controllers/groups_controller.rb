865dc28b944fb335e3ce23ec0b6a87ce0f7f9eb8
Rafael Guzman <rega1237@gmail.com>
2022-09-08 13:18:13 -0400

    controllers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,17 +1,29 @@
 class GroupsController < ApplicationController
+  before_action :redirect_to_splash
+  load_and_authorize_resource
   def index
     @groups = Group.includes([:classifies]).where(author: current_user)
   end
 
   def show
     @group = Group.find(params[:id])
-    @expenses = @group.expenses
+    @expenses = @group.expenses.order(created_at: :desc)
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.author = current_user
+    if @group.save
+      redirect_to @group
+    else
+      render :new
+    end
   end
 
   private
 
   def group_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, :icon)
   end
 
 end
--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,5 +1,4 @@
 class SplashController < ApplicationController
-  skip_before_action :authenticate_user!
   def index 
     redirect_to groups_path if user_signed_in?
   end
